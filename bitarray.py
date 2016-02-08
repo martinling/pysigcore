9d06d930edb8dd7854ec478460155c63d53c6bf7
Martin Ling <martin-git@earth.li>
2016-02-08 23:53:24 +0000

    Implement multidimensional slicing.

--- bitarray.py
+++ bitarray.py
@@ -6,8 +6,8 @@ class BitArray():
     def __init__(self, buf, offset, shape, strides):
         self.buf = np.frombuffer(buf, dtype=np.uint8)
         self.offset = offset
-        self.shape = shape
-        self.strides = strides
+        self.shape = tuple(shape)
+        self.strides = tuple(strides)
         self.ndim = len(shape)
 
     def __repr__(self):
@@ -15,43 +15,56 @@ class BitArray():
             self.__class__.__name__,
                 repr(self.buf), self.offset, self.shape, self.strides)
 
-    def __getitem__(self, index):
-        length = self.shape[0]
-        stride = self.strides[0]
-        if isinstance(index, Integral):
-            # Simple index into first dimension.
-            if index > length - 1 or index < -length:
-                raise IndexError("Index out of range")
-            offset = self.offset + (index % length) * stride
-            if self.ndim == 1:
-                # Retrieve individual bit value.
-                byte_offset = offset / 8
-                bit_offset = offset % 8
-                return bool(self.buf[byte_offset] & (1 << bit_offset))
+    def __getitem__(self, indices):
+
+        if not isinstance(indices, tuple):
+            indices = (indices,)
+
+        idim = len(indices)
+
+        if idim < self.ndim:
+            indices = list(indices) + [slice(None)] * (self.ndim - idim)
+
+        offset = self.offset
+        shape = []
+        strides = []
+
+        for index, length, stride in zip(indices, self.shape, self.strides):
+            if isinstance(index, Integral):
+                # Simple index
+                if index > length - 1 or index < -length:
+                    raise IndexError("Index out of range: %d" % index)
+                offset += (index % length) * stride
+            elif isinstance(index, slice):
+                start, stop, step = index.start, index.stop, index.step
+                if start is None:
+                    start = 0
+                elif start < 0:
+                    start %= length
+                if stop is None:
+                    stop = length
+                elif stop < 0:
+                    stop %= length
+                if step is None:
+                    step = 1
+                offset += (start % length) * stride
+                shape.append((stop - start) // step)
+                strides.append(step * stride)
             else:
-                return BitArray(self.buf, offset, self.shape[1:], self.strides[1:])
-        elif isinstance(index, slice):
-            # Slice into first dimension.
-            start, stop, step = index.start, index.stop, index.step
-            if start is None:
-                start = 0
-            elif start < 0:
-                start %= length
-            if stop is None:
-                stop = length
-            elif stop < 0:
-                stop %= length
-            if step is None:
-                step = 1
-            offset = self.offset + (start % length) * stride
-            shape = tuple([(stop - start) // step] + list(self.shape[1:]))
-            strides = tuple([step * stride] + list(self.strides[1:]))
-            return BitArray(self.buf, offset, shape, strides)
-        elif isinstance(index, tuple):
-            # Multidimensional slice. Implement by recursion.
-            if len(index) == 1:
-                return self[index[0]]
-            else:
-                return self[index[0]][index[1:]]
-        else:
-            return IndexError("Index is not an integer, slice or tuple of slices.")
+                raise IndexError("Invalid index: %s" % repr(index))
+
+        byte_offset = offset / 8
+        bit_offset = offset % 8
+
+        if len(shape) == 0:
+            # Retrieve individual bit value.
+            return bool(self.buf[byte_offset] & (1 << bit_offset))
+
+        bit_size = sum((d - 1) * s for d, s in zip(shape, strides)) + 1
+        byte_size = int(np.ceil(bit_size / 8))
+
+        byte_limit = byte_offset + byte_size
+
+        buf = self.buf[byte_offset:byte_limit]
+
+        return BitArray(buf, bit_offset, shape, strides)
--- bittest.py
+++ bittest.py
@@ -3,11 +3,11 @@ import numpy as np
 
 buf = np.arange(10, dtype=np.uint8)
 offset = 0
-shape = (80,)
-strides = (1,)
+shape = (10,8)
+strides = (8,1)
 
 b = BitArray(buf, offset, shape, strides)
 
-print(b[8])
-print(b[8:16])
+print(b[1,0])
+print(b[0,8:16])
 print(b[:])
